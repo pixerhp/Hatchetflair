e349024f5992891dfb775daf71fc9cad5caaaba3
Jcodefox <jcodefox@gmail.com>
2023-03-12 00:21:57 -0500

    Menu layout changes.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,12 +1,12 @@
 extends Control
 
 # Save select screen inputs
-@onready var as_server_toggle: CheckButton = $Menus/SaveSelectScreen/SaveSelect/Toggles/AsServer
-@onready var allow_players_toggle: CheckButton = $Menus/SaveSelectScreen/SaveSelect/Toggles/AllowPlayers
+@onready var as_server_toggle: CheckButton = $Menus/WorldScreen/WorldSelect/Toggles/AsServer
+@onready var allow_players_toggle: CheckButton = $Menus/WorldScreen/WorldSelect/Toggles/AllowPlayers
 
 # Multiplayer select screen inputs
-@onready var server_ip_box: LineEdit = $Menus/MultiplayerScreen/Multiplayer/IP
-@onready var server_name_box: LineEdit = $Menus/MultiplayerScreen/Multiplayer/Name
+@onready var server_ip_box: LineEdit = $Menus/MultiplayerScreen/Multiplayer/HBoxContainer/IP
+@onready var server_name_box: LineEdit = $Menus/MultiplayerScreen/Multiplayer/HBoxContainer/Name
 
 func open_screen(screen_name: String):
 	for child in $Menus.get_children():
@@ -15,6 +15,11 @@ func open_screen(screen_name: String):
 
 func start_game(is_hosting: bool = false) -> void:
 	pass # Replace with function body.
+
+func toggle_multiplayer_joining(value: bool):
+	as_server_toggle.disabled = not value
+	if not value:
+		as_server_toggle.button_pressed = false
 
 func quit():
 	get_tree().quit()
