89fbc77389731e2a6318da285c9a2a6576f93a14
Jcodefox <jcodefox@gmail.com>
2023-03-13 02:23:05 -0400

    Started adding networking code.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,18 +8,21 @@ extends Control
 @onready var server_ip_box: LineEdit = $Menus/MultiplayerScreen/Multiplayer/HBoxContainer/IP
 @onready var server_name_box: LineEdit = $Menus/MultiplayerScreen/Multiplayer/HBoxContainer/Name
 
-func open_screen(screen_name: String):
+func open_screen(screen_name: String) -> void:
 	for child in $Menus.get_children():
 		child.visible = false
 	get_node("Menus/" + screen_name).visible = true
 
 func start_game(is_hosting: bool = false) -> void:
-	NetworkManager.start_game(not as_server_toggle.button_pressed or not is_hosting, is_hosting, allow_players_toggle.button_pressed or not is_hosting)
+	if is_hosting:
+		NetworkManager.start_game(not as_server_toggle.button_pressed, true, allow_players_toggle.button_pressed)
+	else:
+		NetworkManager.start_game(true, false, true, server_ip_box.text)
 
-func toggle_multiplayer_joining(value: bool):
+func toggle_multiplayer_joining(value: bool) -> void:
 	as_server_toggle.disabled = not value
 	if not value:
 		as_server_toggle.button_pressed = false
 
-func quit():
+func quit() -> void:
 	get_tree().quit()
--- scripts/network_manager.gd
+++ scripts/network_manager.gd
@@ -1,4 +1,59 @@
 extends Node
 
-func start_game(is_player: bool, is_server: bool, allow_others: bool):
-	print(is_player, is_server, allow_others)
+func start_game(is_player: bool, is_server: bool, allow_others: bool, ip_address: String = "") -> void:
+	if is_server:
+		start_server()
+		if is_player:
+			player_connected(multiplayer.get_unique_id())
+		multiplayer.multiplayer_peer.refuse_new_connections = not allow_others
+	else:
+		start_client(ip_address)
+
+func start_client(ip: String) -> void:
+	var peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+	multiplayer.connected_to_server.connect(self.connected_to_server)
+	multiplayer.server_disconnected.connect(self.disconnected_from_server)
+	multiplayer.connection_failed.connect(self.connection_failed)
+	multiplayer.auth_callback = authenticate
+	peer.create_client(ip, 1324)
+	multiplayer.set_multiplayer_peer(peer)
+	print("Client start")
+
+func connected_to_server():
+	print("Connected")
+
+func disconnected_from_server():
+	print("Disconnected from server")
+
+func connection_failed():
+	print("Connection failed")
+
+func start_server() -> void:
+	var peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+	multiplayer.peer_connected.connect(self.player_connected)
+	multiplayer.peer_disconnected.connect(self.player_disconnected)
+	multiplayer.peer_authenticating.connect(self.peer_authenticating)
+	multiplayer.auth_callback = authenticate
+	peer.create_server(1324)
+	multiplayer.set_multiplayer_peer(peer)
+	
+	print("Server started")
+
+func peer_authenticating(id: int) -> void:
+	print("Player %d is authenticating..."%id)
+	multiplayer.send_auth(id, [0])
+
+func player_connected(id: int) -> void:
+	print("Player (" + str(id) + ") connected")
+
+func player_disconnected(id: int) -> void:
+	print("Player (" + str(id) + ") disconnected")
+
+func authenticate(id: int, data: PackedByteArray) -> void:
+	if id == 1:
+		multiplayer.send_auth(id, [1, 3, 3, 7])
+		multiplayer.complete_auth(id)
+	else:
+		if data[0] == 1 and data[1] == 3 and data[2] == 3 and data[3] == 7:
+			multiplayer.complete_auth(id)
+			print("Player %d passed authentication"%id)
