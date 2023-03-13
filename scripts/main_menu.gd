4b5f9af4288ce0d1d280524dfe314cb5c74860d1
Jcodefox <jcodefox@gmail.com>
2023-03-13 15:09:59 -0400

    Commented all gd files.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -8,21 +8,25 @@ extends Control
 @onready var server_ip_box: LineEdit = $Menus/MultiplayerScreen/Multiplayer/HBoxContainer/IP
 @onready var server_name_box: LineEdit = $Menus/MultiplayerScreen/Multiplayer/HBoxContainer/Name
 
+# Open one of the screens on the menu and close all others.
 func open_screen(screen_name: String) -> void:
 	for child in $Menus.get_children():
 		child.visible = false
 	get_node("Menus/" + screen_name).visible = true
 
+# Host or join a game.
 func start_game(is_hosting: bool = false) -> void:
 	if is_hosting:
 		NetworkManager.start_game(not as_server_toggle.button_pressed, true, allow_players_toggle.button_pressed)
 	else:
 		NetworkManager.start_game(true, false, true, server_ip_box.text)
 
+# Disable the as_server_toggle if multiplaer joining is turned off.
 func toggle_multiplayer_joining(value: bool) -> void:
 	as_server_toggle.disabled = not value
 	if not value:
 		as_server_toggle.button_pressed = false
 
+# Close the game.
 func quit() -> void:
 	get_tree().quit()
--- scripts/network_manager.gd
+++ scripts/network_manager.gd
@@ -1,59 +1,89 @@
 extends Node
 
+# Called from the menu to start a server or join a server.
 func start_game(is_player: bool, is_server: bool, allow_others: bool, ip_address: String = "") -> void:
 	if is_server:
 		start_server()
 		if is_player:
+			# Spawn a player for the host.
 			player_connected(multiplayer.get_unique_id())
 		multiplayer.multiplayer_peer.refuse_new_connections = not allow_others
 	else:
 		start_client(ip_address)
 
+###########################
+# Client code
+###########################
+
+# Called when the client is starting.
 func start_client(ip: String) -> void:
 	var peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+	# Setup signals and callbacks for client.
 	multiplayer.connected_to_server.connect(self.connected_to_server)
 	multiplayer.server_disconnected.connect(self.disconnected_from_server)
 	multiplayer.connection_failed.connect(self.connection_failed)
 	multiplayer.auth_callback = authenticate
+	# Connect to the server.
 	peer.create_client(ip, 1324)
 	multiplayer.set_multiplayer_peer(peer)
 	print("Client start")
 
+# Called once authentication of this client is complete.
 func connected_to_server():
 	print("Connected")
 
+# Called in any situation where the client is disconnected from the server.
 func disconnected_from_server():
 	print("Disconnected from server")
 
+# Called upon failing to connect to the server.
 func connection_failed():
 	print("Connection failed")
 
+###########################
+# Server code
+###########################
+
+# Called when the server is starting.
 func start_server() -> void:
 	var peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+	# Setup signals and callbacks for server.
 	multiplayer.peer_connected.connect(self.player_connected)
 	multiplayer.peer_disconnected.connect(self.player_disconnected)
 	multiplayer.peer_authenticating.connect(self.peer_authenticating)
 	multiplayer.auth_callback = authenticate
+	# Create the server.
 	peer.create_server(1324)
 	multiplayer.set_multiplayer_peer(peer)
-	
 	print("Server started")
 
-func peer_authenticating(id: int) -> void:
-	print("Player %d is authenticating..."%id)
-	multiplayer.send_auth(id, [0])
-
+# Called when a client finishes authentication.
+# Also called when the host wants a player to be spawned for itself.
 func player_connected(id: int) -> void:
 	print("Player (" + str(id) + ") connected")
 
+# Called when a client disconnects.
 func player_disconnected(id: int) -> void:
 	print("Player (" + str(id) + ") disconnected")
 
-func authenticate(id: int, data: PackedByteArray) -> void:
-	if id == 1:
-		multiplayer.send_auth(id, [1, 3, 3, 7])
-		multiplayer.complete_auth(id)
-	else:
+###########################
+# Authentication code
+###########################
+
+# Run on the server when a client starts the authentication process.
+func peer_authenticating(id: int) -> void:
+	print("Player %d is authenticating..."%id)
+	# Ask the client to authenticate.
+	multiplayer.send_auth(id, [0])
+
+# Does the actual authenticating.
+func authenticate(sender_id: int, data: PackedByteArray) -> void:
+	if sender_id == 1: # If the sender_id is the server, then this is running as a client.
+		# Send the verification code
+		multiplayer.send_auth(sender_id, [1, 3, 3, 7])
+		multiplayer.complete_auth(sender_id)
+	else: # If the sender_id is not the server, then this is running as the server.
+		# Check the incoming verification code.
 		if data[0] == 1 and data[1] == 3 and data[2] == 3 and data[3] == 7:
-			multiplayer.complete_auth(id)
-			print("Player %d passed authentication"%id)
+			multiplayer.complete_auth(sender_id)
+			print("Player %d passed authentication"%sender_id)
