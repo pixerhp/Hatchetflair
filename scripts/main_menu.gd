01e095ea4f2c1d7eed754fb92960d593d68c224d
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2023-07-27 17:06:47 -0400

    Organization

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -15,6 +15,7 @@ func _ready() -> void:
 # Called every frame
 func _process(_delta: float) -> void:
 	# Code to handle some shortcuts to quickly start the game for testing.
+	# As of typing this, the keys are F1 and F2.
 	if Input.is_action_just_pressed("QuickstartGame"):
 		as_server_toggle.button_pressed = false
 		allow_players_toggle.button_pressed = true
