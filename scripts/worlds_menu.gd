4625f536c49226f51948939cb089765ffa0f4201
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2023-07-29 21:21:07 -0400

    Upgraded "delete world" functionality
    I added a popup so that you don't unintentionally delete worlds, and also preemptively avoided some bugs with it, including:
    
    - being able to have the "new world" and "delete world" popups open at the same time;
    
    - changing which world is selected whilst in the deletion confirmation popup.

--- scripts/worlds_menu.gd
+++ scripts/worlds_menu.gd
@@ -5,14 +5,17 @@ extends Control
 
 var worlds_list = ["bojler", "eladó"]
 @onready var worlds_text = get_node("WorldsScreenUI/Worlds")
-@onready var new_world = get_node("WorldsScreenUI/WorldButtons/NewWorld")
+@onready var new_world_button = get_node("WorldsScreenUI/WorldButtons/NewWorld")
 @onready var new_world_popup = get_node("NewWorldPopup")
+@onready var delete_world_popup = get_node("DeleteWorldPopup")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_world.pressed.connect(self.open_new_world_popup)
+	new_world_button.pressed.connect(self.open_new_world_popup)
 	new_world_popup.get_node("Okay").pressed.connect(self.confirm_new_world)
 	new_world_popup.get_node("Cancel").pressed.connect(new_world_popup.hide)
+	delete_world_popup.get_node("Confirm").pressed.connect(self.confirm_delete_world)
+	delete_world_popup.get_node("Cancel").pressed.connect(delete_world_popup.hide)
 	update_worlds_list()
 
 # Disables the host_without_playing_toggle if multiplaer joining is turned off.
@@ -23,6 +26,7 @@ func toggle_multiplayer_joining(button_value: bool) -> void:
 
 # Open the new world popup.
 func open_new_world_popup():
+	delete_world_popup.hide()
 	new_world_popup.get_node("TextEdit").clear()
 	new_world_popup.show()
 
@@ -32,9 +36,21 @@ func confirm_new_world():
 	update_worlds_list()
 	new_world_popup.hide()
 
-func delete_world():
-	worlds_list.erase(worlds_list[worlds_text.get_selected_items()[0]])
+func open_delete_world_popup():
+	new_world_popup.hide()
+	if not worlds_text.get_selected_items().is_empty(): # Don't do anything if no worlds are selected.
+		delete_world_popup.get_node("RichTextLabel").text = "[center]Are you sure you want to delete \"" + worlds_list[worlds_text.get_selected_items()[0]] +"\"?\n(This action cannot be undone.)[/center]"
+		delete_world_popup.show()
+
+func confirm_delete_world():
+	if not worlds_text.get_selected_items().is_empty(): # Crash prevention for if no world is selected.
+		worlds_list.erase(worlds_list[worlds_text.get_selected_items()[0]])
+		delete_world_popup.hide()
 	update_worlds_list()
+
+# This is done to prevent changing which item you're deleting after the deletion popup already named a world.
+func world_list_item_clicked():
+	delete_world_popup.hide()
 
 # Update the visible list for the player.
 func update_worlds_list():
