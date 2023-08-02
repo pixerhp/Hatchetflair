3977292dfc9f48446e7355ba74ec6cb895becb98
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2023-08-02 17:16:42 -0400

    More join menu code progress

--- scripts/menu_join.gd
+++ scripts/menu_join.gd
@@ -1,8 +1,8 @@
 extends Control
 
 @onready var servers_list_text = get_node("JoinScreenUI/SavedServersList")
-var servers_names = []
-var servers_ips = []
+var servers_nicknames = ["server a", "server b"]
+var servers_ips = ["0", "0"]
 
 
 # Called when the node enters the scene tree for the first time.
@@ -20,6 +20,28 @@ func join_server(server_index: int = 0):
 func _on_join_button_pressed():
 	if not servers_list_text.get_selected_items().is_empty(): # Don't do anything if no worlds are selected.
 		join_server()
+
+
+func open_add_server_popup():
+	hide_all_servers_menu_popups()
+	var add_server_popup = get_node("AddServerPopup")
+	add_server_popup.get_node("ServerIPInput").clear()
+	add_server_popup.get_node("ServerNicknameInput").clear()
+	add_server_popup.show()
+
+func confirm_new_server():
+	var add_server_popup = get_node("AddServerPopup")
+	servers_ips.append(add_server_popup.get_node("ServerIPInput").text)
+	servers_nicknames.append(add_server_popup.get_node("ServerNicknameInput").text)
+	update_servers_list_text()
+	add_server_popup.hide()
+
+
+# Update the text of the visible servers-list for the player.
+func update_servers_list_text():
+	servers_list_text.clear()
+	for server_name in servers_nicknames:
+		servers_list_text.add_item(server_name)
 
 
 func _on_servers_list_item_selected():
