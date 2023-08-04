4cf1fdb23d84daf7ff808a11e8b117a3416309a2
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2023-08-04 18:24:47 -0400

    Minor organization

--- scripts/global_hotkeys.gd
+++ scripts/global_hotkeys.gd
@@ -1,13 +1,8 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+# Called every frame.
+func _process(_delta):
 	if Input.is_action_just_pressed("Fullscreen Toggle") :
 		if (DisplayServer.window_get_mode() == 0):
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
