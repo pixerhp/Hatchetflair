e46635119df8915cfb7e8c39e08a51bf9da2bfe2
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2023-08-07 14:58:31 -0400

    Updated version number stuff
    (I also updated the splashes a tiny bit.)

--- scripts/autoload_scripts/global_stuff.gd
+++ scripts/autoload_scripts/global_stuff.gd
@@ -1,42 +1,46 @@
 extends Node
 
 const game_version_phase: String = "1"
-const game_version_major: String = "pregame"
+const game_version_major: String = "pre-game"
 const game_version_minor: String = "6"
-
-func version_entire_string() -> String:
-	return(game_version_phase + "." + game_version_major + "." + game_version_minor)
+const game_version_entire: String = game_version_phase + "." + game_version_major + "." + game_version_minor
 
 
 func _ready() -> void:
 	# Ensure the global random number generator isn't the same every program execution.
 	randomize()
+	
+	# Set the windows title to include the game's name, the version, and a fun splash text if it loads right.
+	set_window_title()
 
 
-func set_window_title():
+func set_window_title(optional_specified_title: String = ""):
+	if not (optional_specified_title == ""):
+		DisplayServer.window_set_title(optional_specified_title)
+		return
 	# Preemptively set the window's title, just in case something goes wrong with splash texts.
-	DisplayServer.window_set_title("Hatchetflare - " + version_entire_string())
+	DisplayServer.window_set_title("Hatchetflare   v" + game_version_entire)
 	# Attempt to open the splash texts file.
 	var splash_texts_file = FileAccess
 	if splash_texts_file.file_exists("res://assets/text_files/window_splash_texts.txt"):
 		# Create an array of usable splash texts.
 		splash_texts_file = FileAccess.open("res://assets/text_files/window_splash_texts.txt", FileAccess.READ)
-		var line_number: int = 0
 		var line_contents: String = ""
 		var usable_splash_texts: Array[String] = []
 		while (splash_texts_file.eof_reached() == false) :
-			line_number += 1
 			line_contents = splash_texts_file.get_line()
 			if (not line_contents.begins_with("\t")) and (line_contents != ""):
 				usable_splash_texts.append(line_contents)
 		splash_texts_file.close()
 		# Set the title to include a splash text, if the usable splash texts array has at least one item.
 		if usable_splash_texts.is_empty():
-			push_warning("The splash texts file was successfully accessed, but the resulting list of splashes is empty, so no splash text was used.")
+			push_warning("The window title splash texts file was accessed successfully, but no useable splashes were found, so no splash text was used.")
 		else:
-			DisplayServer.window_set_title("Hatchetflare - " + version_entire_string() + " - " + usable_splash_texts[randi() % usable_splash_texts.size()])
+			DisplayServer.window_set_title("Hatchetflare   v" + game_version_entire + "   ---   " + usable_splash_texts[randi() % usable_splash_texts.size()])
 	else:
-		push_warning("The file for window splash texts was not found in the menu_main script, so no splash text was used.")
+		push_warning("The file for window title splash texts was not found, so no splash text was used.")
+
+
 
 # Called every frame.
 func _process(_delta):
