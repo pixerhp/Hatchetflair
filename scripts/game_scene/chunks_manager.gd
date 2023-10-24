39f5a7a5b9f34bad06940c38f73ace0bf81310d4
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2023-10-24 12:34:49 -0400

    Basic static chunk object gen progress

--- scripts/game_scene/chunks_manager.gd
+++ scripts/game_scene/chunks_manager.gd
@@ -1,11 +1,60 @@
 extends Node3D
 
+const MAX_NEW_STATIC_CHUNKS_BATCH_SIZE = 64
+var do_chunk_generating: bool = true
+var new_static_chunks_queue: Array[Vector3i] = []
+var new_chunks: Array[StaticBody3D] = [] # Used for giving other threads references to the new chunk nodes.
+var static_chunk_prefab: PackedScene = preload("res://scenes/prefabs/terrain/chunk_static.tscn")
+@onready var chunk_gen_thread: Thread = Thread.new()
 
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-#	pass # Replace with function body.
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
+func _ready():
+	for h in range(-4, 8):
+		for x1 in range(-4, 8):
+			for x2 in range(-4, 8):
+				new_static_chunks_queue.append(Vector3i(h, x1, x2))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if not do_chunk_generating:
+		return
+	if chunk_gen_thread.is_started() and not chunk_gen_thread.is_alive():
+		chunk_gen_thread.wait_to_finish()
+		new_chunks.clear()
+		print("children after: ", self.get_child_count())
+	if not chunk_gen_thread.is_started():
+		determine_new_static_chunks()
+	if not new_static_chunks_queue.is_empty():
+		print("children before: ", self.get_child_count())
+		if new_static_chunks_queue.size() > MAX_NEW_STATIC_CHUNKS_BATCH_SIZE:
+			add_static_chunks(new_static_chunks_queue.slice(0, MAX_NEW_STATIC_CHUNKS_BATCH_SIZE).duplicate())
+			new_static_chunks_queue = new_static_chunks_queue.slice(MAX_NEW_STATIC_CHUNKS_BATCH_SIZE)
+		else:
+			add_static_chunks(new_static_chunks_queue.duplicate())
+			new_static_chunks_queue.clear()
+	remove_chunks()
+	return
+
+func determine_new_static_chunks():
+	pass
+func add_static_chunks(chunk_coords_hxx_queue: Array[Vector3i]):
+	if chunk_coords_hxx_queue.is_empty():
+		return
+	new_chunks.resize(chunk_coords_hxx_queue.size())
+	for i in chunk_coords_hxx_queue.size():
+		new_chunks[i] = static_chunk_prefab.instantiate()
+		new_chunks[i].name = str(chunk_coords_hxx_queue[i])
+		new_chunks[i].chunk_coords_hxx = chunk_coords_hxx_queue[i]
+		new_chunks[i].chunk_length = 16
+		new_chunks[i].position = new_chunks[i].chunk_coords_hxx * new_chunks[i].chunk_length
+		self.add_child(new_chunks[i])
+	chunk_gen_thread.start(add_static_chunks_threadwork)
+	return
+func add_static_chunks_threadwork():
+	pass
+
+func determine_chunks_to_remove():
+	pass
+func remove_chunks():
+	pass
