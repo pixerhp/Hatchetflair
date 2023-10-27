089261dbdbfb1121f0b7d09a67d2020d41b82f65
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2023-10-26 20:27:46 -0400

    Implimented not-yet-functional pause menu UI
    Currently it can be toggled by pressing the escape key whilst in the 3D gameplay environment.

--- scripts/game_scene/game_scene.gd
+++ scripts/game_scene/game_scene.gd
@@ -1,11 +1,16 @@
 extends Node3D
 
 
-## Called when the node enters the scene tree for the first time.
-#func _ready():
-#	pass # Replace with function body.
-#
-#
-## Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("Pause Menu"):
+		var pause_menu_node: Node = $PauseMenu
+		if pause_menu_node.visible:
+			pause_menu_node.visible = false
+		else:
+			pause_menu_node.visible = true
