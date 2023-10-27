7fb20d1f729a8c479b144c9c898f22ff086711da
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2023-10-26 20:45:33 -0400

    Minor organization & code refining

--- scripts/game_scene/game_scene.gd
+++ scripts/game_scene/game_scene.gd
@@ -1,16 +1,9 @@
 extends Node3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
+	# Toggle the pause menu if its associated key is pressed.
+	# !!! [in the future, esc should also be able to close out of other things WITHOUT opening this menu.]
 	if Input.is_action_just_pressed("Pause Menu"):
-		var pause_menu_node: Node = $PauseMenu
-		if pause_menu_node.visible:
-			pause_menu_node.visible = false
-		else:
-			pause_menu_node.visible = true
+		$PauseMenu.visible = not $PauseMenu.visible
