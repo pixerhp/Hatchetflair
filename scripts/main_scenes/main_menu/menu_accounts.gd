f32eef53a75fd82d840f77bbbb8224d70f570227
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2023-11-15 13:48:51 -0500

    Very minor accounts menu progress

--- scripts/main_scenes/main_menu/menu_accounts.gd
+++ scripts/main_scenes/main_menu/menu_accounts.gd
@@ -1,6 +1,11 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
+
+func _on_account_option_button_item_selected(selected_index: int):
+	# !!! since a new account was selected from the dropdown, 
+	# refresh everything that that should affect accordingly.
+	print("selected account with index: ", selected_index)
+	pass
