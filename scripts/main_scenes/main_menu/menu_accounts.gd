91d08aa955b5c96cd17a184f6788d20dcda85c82
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2023-12-04 17:11:48 -0500

    Changes to edit GUEST displayname functionality

--- scripts/main_scenes/main_menu/menu_accounts.gd
+++ scripts/main_scenes/main_menu/menu_accounts.gd
@@ -38,10 +38,6 @@ func _update_edit_account_button_disabledness():
 	if (edit_account_button_node == null) or (account_select_node == null):
 		push_error("At least one of two nodes not found.")
 		return
-	if account_select_node.selected == 0:
-		edit_account_button_node.disabled = true
-	else:
-		edit_account_button_node.disabled = false
 	return
 
 
@@ -53,10 +49,7 @@ func _on_account_option_button_item_selected(index_of_selected: int):
 		push_error("Account select option-button node not found.")
 		return
 	Globals.player_username = account_select_node.get_item_text(index_of_selected)
-	if not Globals.player_username == "GUEST":
-		Globals.player_displayname = "[" + Globals.player_username + "'s displayname]"
-	else:
-		Globals.player_displayname = "Guest"
+	Globals.player_displayname = "[" + Globals.player_username + "'s displayname]"
 	_update_account_names_text()
 	return
 
@@ -66,7 +59,16 @@ func _on_add_account_pressed():
 	account_popup_node.visible = true
 	general_menu_nodes_container.visible = false
 func _on_edit_account_pressed():
-	account_popup_titletext_node.text = "[center]Edit account username and displayname.[/center]"
+	var account_select_node: OptionButton = $VBoxContainer/AccountSelectContainer/AccountSelect
+	if account_select_node == null:
+		push_error("Account select option-button node not found.")
+		return
+	if account_select_node.selected == 0:
+		account_popup_titletext_node.text = "[center]Edit current guest displayname.[/center]"
+		account_popup_contents.get_node("UsernameInput").visible = false
+	else:
+		account_popup_titletext_node.text = "[center]Edit account username and displayname.[/center]"
+		account_popup_contents.get_node("UsernameInput").visible = true
 	
 	account_popup_node.visible = true
 	general_menu_nodes_container.visible = false
