2831acc7dcbf8ed1dd087925a388c9fa5b1a9e20
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2023-12-11 14:50:56 -0500

    minor random touchups

--- scripts/main_scenes/main_menu/menu_title.gd
+++ scripts/main_scenes/main_menu/menu_title.gd
@@ -10,30 +10,32 @@ func _refresh_texts():
 	return
 
 func _update_corner_version_text():
+	# Find the text node.
 	var corner_version_text_node: RichTextLabel = $TitleUI/HBoxContainer/VersionText
 	if corner_version_text_node == null:
 		push_error("Corner version text node not found.")
 		return
+	
 	corner_version_text_node.text = "v" + Globals.V_ENTIRE
 	return
+
 func _update_welcome_message():
+	# Find the text node that displays the welcome message.
 	var welcome_message_node: RichTextLabel = $TitleUI/WelcomeMessage
 	if welcome_message_node == null:
 		push_error("Welcome message text node not found.")
 		return
-	if not Globals.player_username == "GUEST":
-		welcome_message_node.text = (
-			"[center]" +
-			"Welcome, " + 
-			Globals.player_displayname + " (@" + Globals.player_username + ")" +
-			"[/center]" )
-	else:
-		welcome_message_node.text = (
-			"[center]" +
-			"Welcome, " + 
-			Globals.player_displayname + " (@" + Globals.player_username + ")" +
-			"\n(you are playing as a GUEST account.)" +
-			"[/center]" )
+	
+	# Set up the titlescreen's user welcome message.
+	welcome_message_node.text = (
+		"[center]" +
+		"Welcome, " + 
+		Globals.player_displayname + " (@" + Globals.player_username + ")"
+	)
+	if Globals.player_username == "GUEST":
+		welcome_message_node.text += "\n(you are playing as a GUEST account.)"
+	welcome_message_node.text += "[/center]"
+	
 	return
 
 
