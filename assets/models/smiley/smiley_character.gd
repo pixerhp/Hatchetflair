2647596b14340ca1f084fdbcb79d9e4861f53e9f
Pixer Pinecone <99830732+PixerPinecone@users.noreply.github.com>
2024-06-04 12:06:15 -0400

    smiley characters walk around randomly
    note: they don't actually face the direction that they're walking in yet.

---
+++ assets/models/smiley/smiley_character.gd
@@ -0,0 +1,42 @@
+extends CharacterBody3D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var character: CharacterBody3D = self
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+const WALK_SPEED = 5.0
+
+func _ready():
+	animation_player.set_assigned_animation("walk")
+
+var action_timer: int = 180
+enum actions_enums {IDLE, WALK}
+var action_type: int = actions_enums.IDLE
+var walk_direction: Vector2 = Vector2.ZERO
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	action_timer -= 1
+	if action_timer <= 0:
+		action_timer = randi_range(20, 400)
+		action_type = actions_enums.values().pick_random()
+		
+		match action_type:
+			actions_enums.IDLE:
+				animation_player.set_assigned_animation("walk")
+				animation_player.stop()
+				walk_direction = Vector2.ZERO
+			actions_enums.WALK:
+				animation_player.set_assigned_animation("walk")
+				animation_player.play()
+				walk_direction = (Vector2.UP.rotated(randf_range((-PI), (PI)))).normalized()
+	
+	var direction = (transform.basis * Vector3(walk_direction.x, 0, walk_direction.y)).normalized()
+	velocity.x = direction.x * WALK_SPEED
+	velocity.z = direction.z * WALK_SPEED
+	
+	move_and_slide()
