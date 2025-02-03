bee52fdbf80936e4a590aa2fad9a83ee832e4d2b
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2025-02-03 12:15:37 -0500

    Update ChemCraft substance data structure

--- scripts/autoloaded/chem_craft.gd
+++ scripts/autoloaded/chem_craft.gd
@@ -3,22 +3,66 @@
 extends Node
 
 
+# Substance instances have unique crafing/chemistry, 
+# and point to a SubsProps instance for their properties.
 class Substance:
 	var names: PackedStringArray = [] # A substance's "main"/"default" name is the one listed first.
 	var tags: PackedInt32Array = []
-	var density: float = 1 # density in [standardized weight unit] per cubic metrin.
+	var subsprop_i: int = -1
 	# !!! add rendering and mesh related info / references to info
+	# !!! mesh detailing style (index?)
 	
 	func _init(
 		in_names: PackedStringArray, 
 		in_tags: PackedInt32Array,
-		in_density: float,
+		in_subsprop_i: int,
 	):
 		names.clear()
 		names.append_array(in_names)
 		tags.clear()
 		tags.append_array(in_tags)
-		density = in_density
+		subsprop_i = in_subsprop_i
+	
+	func get_properties() -> SubsProp:
+		if subsprop_i == -1:
+			push_error("This Substance has no associated SubsProp index.")
+			return ChemCraft.bad_subsprop
+		else:
+			return ChemCraft.subsprops[subsprop_i]
+
+# Substance instances are separated from their properties so that multiple substances can share properties. 
+# For example, the dyes have different crafting/chemistry/rendering, but can share physical properties.
+class SubsProp:
+	var name: String = "" 
+		# Used not in functional gameplay, but in ease of linking substances to their properties.
+	
+	# not state of matter dependant:
+	var ion_emmission_color: Color 
+	
+	# solid
+	var s_density: float = 1 # density in [standardized weight unit] per cubic metrin.
+	var s_elec_resis: float = 1 # 0 is a valid input, representing electrical superconductors.
+	var s_hardness: float = 1
+	
+	# liquid
+	var l_density: float  = 1
+	var l_elec_resis: float = 1 # 0 is a valid input, representing electrical superconductors.
+	var l_viscosity: float = 1
+	# cohesion & adhesion?
+	
+	# gas/vapor
+	var g_density: float = 1
+	var g_elec_resis: float = 1 # 0 is a valid input, representing electrical superconductors.
+	
+	# thermal conductivity/insulation?
+	
+	# !!! Add full initialization later.
+	func _init(
+		in_name: String,
+	):
+		name = in_name
+
+var bad_subsprop: SubsProp = SubsProp.new("error")
 
 enum MESH_DETAILING_STYLE {
 	BRICKS,
@@ -37,22 +81,16 @@ enum SUBS_TAG { # "substance tag"
 }
 
 var substance_name_to_i: Dictionary = {}
-
 func get_substance(subs_main_name: String) -> Substance:
 	return substances[substance_name_to_i[subs_main_name]]
 
-var substances: Array[Substance] = [
-	Substance.new(
-		["air"],
-		[],
-		1.0, # !!! set later, this value is temporary due to not having decided on a standard mass unit yet.
-	),
-	Substance.new(
-		["ferrium", "ferry", "iron"],
-		[SUBS_TAG.PURE_ELEMENT, SUBS_TAG.METAL],
-		1.0, # !!! set later, this value is temporary due to not having decided on a standard mass unit yet.
-	),
-]
+var subsprop_name_to_i: Dictionary = {}
+func get_subsprop(subsprop_name: String) -> SubsProp:
+	return subsprops[subsprop_name_to_i[subsprop_name]]
+
+var substances: Array[Substance] = []
+
+var subsprops: Array[SubsProp] = []
 
 # These define FUNCTIONAL states of matter rather than describing lore/worldbuilding.
 # Ex. What may in-universe be considered a "plasma" may functionally still be treated as a gas.
@@ -72,7 +110,57 @@ enum STATE_OF_MATTER {
 
 
 func _ready():
-	# Prepare the substance_name_to_i dictionary, which may be used by various code/threads.
+	# Note: the order of these functions matters due to depending on eachother.
+	initialize_subsprops_list()
+	prepare_subsprop_name_to_i_dict()
+	initialize_substance_list()
+	prepare_substance_name_to_i_dict()
+	return
+
+func initialize_subsprops_list() -> void:
+	subsprops.clear()
+	subsprops = [
+		SubsProp.new(
+			"air",
+		),
+		SubsProp.new(
+			"ferrium",
+		),
+		SubsProp.new(
+			"dye",
+		),
+	]
+	return
+
+func prepare_subsprop_name_to_i_dict() -> void:
+	subsprop_name_to_i.clear()
+	for i in subsprops.size():
+		subsprop_name_to_i[subsprops[i].name] = i
+	return
+
+func initialize_substance_list() -> void:
+	substances.clear()
+	substances = [
+		Substance.new(
+			["air"],
+			[],
+			subsprop_name_to_i["air"],
+		),
+		Substance.new(
+			["ferrium", "ferry", "iron"],
+			[SUBS_TAG.PURE_ELEMENT, SUBS_TAG.METAL],
+			subsprop_name_to_i["ferrium"],
+		),
+		Substance.new(
+			["black dye"],
+			[SUBS_TAG.DYE],
+			subsprop_name_to_i["dye"],
+		),
+	]
+	return
+
+func prepare_substance_name_to_i_dict() -> void:
 	substance_name_to_i.clear()
 	for i in substances.size():
 		substance_name_to_i[substances[i].names[0]] = i
+	return
