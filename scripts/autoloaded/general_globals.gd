7de191376ddc5f45c8b375d5756a8440d725e0d7
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2025-07-24 17:15:34 -0400

    Pressing 'esc' quits the game

--- scripts/autoloaded/general_globals.gd
+++ scripts/autoloaded/general_globals.gd
@@ -4,9 +4,12 @@ var draw_debug_chunkborders: bool = true
 
 func _process(_delta):
 	# Process inputs that should work regardless of game state:
+	if Input.is_action_just_pressed("escape"):
+		get_tree().quit()
 	if Input.is_action_just_pressed("fullscreen_toggle"):
 		if not DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
-	# Can later implement Screenshot, toggle Console window, etc...
+	
+	# !!! Can later implement Screenshot, toggle Console window, etc...
