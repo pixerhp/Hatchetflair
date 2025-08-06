96d32d0c90c8193f9e59a7adf4f957f8276e12d6
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2025-08-06 19:34:10 -0400

    Revise existing chunk code

--- scripts/tchunks_manager.gd
+++ scripts/tchunks_manager.gd
@@ -4,7 +4,7 @@ extends Node
 
 # Lengths, totals, and sizes of chunk stuff in metrins.
 const TCHUNK_L: int = 16
-const TCHUNK_T: int = 16 ** 3
+const TCHUNK_T: int = TCHUNK_L ** 3
 const TCHUNK_S: Vector3i = Vector3i(TCHUNK_L, TCHUNK_L, TCHUNK_L)
 const TCHUNK_PAD_L: int = 1
 const TCHUNK_PAD_T: int = (TCHUNK_L + TCHUNK_PAD_L) ** 3
@@ -15,19 +15,15 @@ enum TILE_SHAPE {
 	EMPTY, TESS_CUBE, TESS_RHOMBDO, MARCH_CUBE,
 }
 
-
 class TChunk:
 	var tile_shapes: PackedByteArray = []
+	
 	var mesh_instance_node: MeshInstance3D = MeshInstance3D.new()
 	var array_mesh: ArrayMesh = ArrayMesh.new()
 	
-	func t_i_from_xyz(
-		xyz: Vector3i, lengths: Vector3i = Vector3i(TCHUNK_L, TCHUNK_L, TCHUNK_L)
-	) -> int:
+	func t_i_from_xyz(xyz: Vector3i, lengths: Vector3i = TCHUNK_S) -> int:
 		return xyz.x + (xyz.y * lengths.y) + (xyz.z * lengths.z * lengths.z)
-	func t_xyz_from_i(
-		i: int, lengths: Vector3i = Vector3i(TCHUNK_L, TCHUNK_L, TCHUNK_L),
-	) -> Vector3i:
+	func t_xyz_from_i(i: int, lengths: Vector3i = TCHUNK_S,) -> Vector3i:
 		return Vector3i(
 			posmod(i, lengths.x),
 			posmod(i/lengths.x, lengths.y),
@@ -35,20 +31,21 @@ class TChunk:
 		)
 	
 	func _init():
-		tile_shapes.resize(TCHUNK_L ** 3)
+		tile_shapes.resize(TCHUNK_T)
 	func randomize_tiles():
-		for i in range(TCHUNK_L ** 3):
+		for i in range(TCHUNK_T):
 			tile_shapes[i] = randi_range(0, 1)
 	func unmesh():
+		mesh_instance_node.queue_free()
 		mesh_instance_node = MeshInstance3D.new()
 		array_mesh = ArrayMesh.new()
-	func generate_mesh():
+	func generate_mesh(surr_tchunks: Array[TChunk] = [TChunk.new(), TChunk.new() ,TChunk.new()]):
 		# Get all of the information needed, including data related to surrounding chunks.
 		# For now though, all needed surrounding chunk tile shapes are simply assumed to be empty.
 		var padded_shapes: PackedByteArray = []
 		padded_shapes.resize((TCHUNK_L + 1) ** 3)
 		padded_shapes.fill(TILE_SHAPE.EMPTY)
-		for i in range(TCHUNK_L ** 3):
+		for i in range(TCHUNK_T):
 			padded_shapes[
 				t_i_from_xyz(t_xyz_from_i(i) + Vector3i(1, 1, 1), TCHUNK_PAD_S)
 			] = tile_shapes[i]
@@ -70,10 +67,10 @@ class TChunk:
 							if padded_shapes[
 								t_i_from_xyz(Vector3i(x - 1, y, z), TCHUNK_PAD_S)
 							] == TILE_SHAPE.EMPTY:
+								surf_verts.append(Vector3(x-1, y-1, z-0))
 								surf_verts.append(Vector3(x-1, y-1, z-1))
-								surf_verts.append(Vector3(x-1, y-1, z-2))
+								surf_verts.append(Vector3(x-1, y+0, z-0))
 								surf_verts.append(Vector3(x-1, y+0, z-1))
-								surf_verts.append(Vector3(x-1, y+0, z-2))
 								surf_inds.append_array([
 									surf_verts.size() - 4,
 									surf_verts.size() - 3,
@@ -115,6 +112,8 @@ class TChunk:
 		mesh_instance_node.mesh = array_mesh
 
 func _ready():
+	assert(TCHUNK_PAD_L <= TCHUNK_L)
+	
 	var new_chunk: TChunk = TChunk.new()
 	#new_chunk.randomize_tiles()
 	new_chunk.tile_shapes[0] = TILE_SHAPE.TESS_CUBE
