6350c227b7ff9bf3ca81702054476430c2430352
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2025-09-02 17:48:00 -0400

    Fix normal map direction interpretation

--- scripts/tchunks_manager.gd
+++ scripts/tchunks_manager.gd
@@ -271,8 +271,8 @@ func generate_test_mesh():
 		Vector3(0,1,0), Vector3(0,1,0), Vector3(0,1,0), 
 	])
 	mesh_surface[Mesh.ARRAY_TEX_UV] = PackedVector2Array([
-		Vector2(0,0), Vector2(0,1), Vector2(1,0), 
-		Vector2(1,1), Vector2(1,0), Vector2(0,1), 
+		Vector2(0,1), Vector2(0,0), Vector2(1,1), 
+		Vector2(1,0), Vector2(1,1), Vector2(0,0), 
 	])
 	mesh_surface[Mesh.ARRAY_CUSTOM0] = PackedByteArray([
 		0b00000000, 0b00000000, 0b00000000, 0b00000000, 
