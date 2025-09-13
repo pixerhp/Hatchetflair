68eaa513f3450c344e14fc1eb9d34896e7e7ff0c
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2025-09-13 17:08:32 -0400

    Implement initialize_substances function

--- scripts/autoloaded/chemcraft.gd
+++ scripts/autoloaded/chemcraft.gd
@@ -5,22 +5,25 @@ class PATHS:
 	const albedos_textures: String = subst_assets_path + "albedos/"
 	const normals_textures: String = subst_assets_path + "normals/"
 	const specials_textures: String = subst_assets_path + "specials/"
-	const substs_data: String = subst_assets_path + "substances.csv"
+	const substs_data: String = subst_assets_path + "substances.csv.txt"
 
 class Subst:
 	var name: String = ""
 	# var value: float ?
-	var default_albedo_tex: int = -1
-	var default_vert_color: Color = Color.WHITE
+	var albedo_ind: int = -1
+	var vert_color: Color = Color.WHITE
 	#var alt_albedo_tex: int = -1
 	#var alt_vert_color: Color = Color.WHITE
-	var default_normal_tex: int = -1
+	var normal_ind: int = -1
 	#var alt_normal_tex: int = -1
-	var default_special_tex: int = -1
+	var special_ind: int = -1
 	#var alt_special_tex: int = -1
 
+# !!! can later replace dicts with simple name array, 
+# given that each name corresponds to a single index from 0 to size - 1?
+
 var SUBSTANCES: Array[Subst] = []
-var subst_name_to_i: Dictionary[String,Subst] = {}
+var subst_name_to_i: Dictionary[String,int] = {} 
 
 var albedos_texarray: Texture2DArray = Texture2DArray.new()
 var normals_texarray: Texture2DArray = Texture2DArray.new()
@@ -59,4 +62,20 @@ func initialize_substance_textures():
 		[albedos_texarray,normals_texarray,specials_texarray][i].create_from_images(temp_imagearray.duplicate())
 
 func initialize_substances():
-	pass
+	var file: FileAccess = FileAccess.open(PATHS.substs_data, FileAccess.READ)
+	var csv_data: PackedStringArray = []
+	while file.get_position() < file.get_length():
+		csv_data = file.get_csv_line(",")
+		if csv_data.size() < 2: 
+			continue # (skip over empty/placeholder data.)
+		if csv_data.size() != 7:
+			push_warning("Bad substances csv data: ", csv_data)
+			continue
+		var new_subst: Subst = Subst.new()
+		new_subst.name = csv_data[0]
+		new_subst.albedo_ind = albedos_name_to_i.get(csv_data[1], -2)
+		new_subst.vert_color = Color(float(csv_data[2]), float(csv_data[3]), float(csv_data[4]))
+		new_subst.normal_ind = normals_name_to_i.get(csv_data[5], -2)
+		new_subst.special_ind = specials_name_to_i.get(csv_data[6], -2)
+		subst_name_to_i[new_subst.name] = SUBSTANCES.size()
+		SUBSTANCES.append(new_subst)
