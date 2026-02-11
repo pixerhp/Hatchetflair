d7c8f59b155efcf4ac9fe366089111f71ea23c75
Pixer Pinecone <99830732+pixerhp@users.noreply.github.com>
2026-02-10 22:59:54 -0500

    WorldChunk class testing

--- godot_extension/src/chunks_manager.cpp
+++ godot_extension/src/chunks_manager.cpp
@@ -1,11 +1,18 @@
 #include "chunks_manager.h"
 #include "world_utils.h"
 
+class WorldChunk {
+	public:
+		unsigned char tile_shapes[4096];
+};
+
 void ExampleClass::_bind_methods() {
 	godot::ClassDB::bind_method(D_METHOD("print_type", "variant"), &ExampleClass::print_type);
 }
 
 void ExampleClass::print_type(const Variant &p_variant) const {
+	WorldChunk test_chunk = WorldChunk();
 	print_line(vformat("Type: %d", p_variant.get_type()));
 	print_line(vformat("Hello! %d %d", WorldUtils::get_instance().testval, WorldUtils::get_instance().undefinedint));
+	print_line(vformat("Some tile shapes: %d %d", test_chunk.tile_shapes[0], test_chunk.tile_shapes[4100]));
 }
