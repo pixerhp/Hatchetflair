c335cd0ce31ea50236d53e8501c9685d0bf96742
Pixer H. P. <99830732+pixerhp@users.noreply.github.com>
2026-06-29 17:42:44 -0400

    Update Input Map actions

--- godot_project/scripts/controls_ui.gd
+++ godot_project/scripts/controls_ui.gd
@@ -1,5 +1,5 @@
 extends PanelContainer
 
 func _process(_delta: float) -> void:
-	if Input.is_action_just_pressed("controls_toggle"):
+	if Input.is_action_just_pressed("debug_controls"):
 		visible = not visible
